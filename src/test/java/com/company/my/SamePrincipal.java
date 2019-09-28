0096330c0d918f5b09e87c3144458428e3bde4ae
walker <15108309406@yeah.net>
2019-09-28 21:10:43 +0800

    init

---
+++ src/test/java/com/company/my/SamePrincipal.java
@@ -0,0 +1,16 @@
+package com.company.my;
+
+import org.junit.Test;
+
+/**
+ * 等额本金计算方式.
+ * Created by walker on 2019/9/28.
+ */
+public class SamePrincipal {
+
+    @Test
+    public void testPrincipal() {
+        System.out.println("等额本金计算demo");
+
+    }
+}
